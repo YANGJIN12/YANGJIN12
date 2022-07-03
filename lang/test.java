585af101dbc4a4b259e1210b9a65df63141afb1d
김양진 <107318969+YANGJIN12@users.noreply.github.com>
2022-07-04 01:02:22 +0900

    Add files via upload

---
+++ lang/test.java
@@ -0,0 +1,60 @@
+package lang;
+
+public class test {
+	public static void main(String[] args) {
+//		String, StringBuffer
+//		Process, Runtime, Thread Math, StrictMath
+//		Exception Throwable, Error Package, Class, ClassLoader
+//		Wrapper, System, Stream
+//		lang 패키지에서 사용되는 클래스	
+	
+//		class 클래스 대표적인 메소드
+		forName() 
+		genName() 
+		newInstance() 
+		getSuperclass() 
+		getFields()
+
+// 		String 클래스
+		compareTo() 
+		indexOf() 
+		charAt() 
+		substring() 
+		replace() : 
+		toUpperCase(), toLowerCase() 
+		valueOf() 
+		equals() 
+		toString() 
+		
+// 		Wrapper 클래스
+		parseInt() 
+		valueOf() 
+		
+//		Math 클래스 
+
+static int abs(int a)	a의 절대값을 int로 반환한다.
+static double ceil(double a)	
+static double floor(double a)	
+static int max(int a, int b)	
+static int min(int a, int b)	
+static double random()	
+static double round(double a)	
+static double rint(double a)	
+static double sin(double a)	
+static double cos(double a)
+static double tan(double a)	
+static double sqrt(double a)
+		
+//		Runtime 클래스
+
+Process exec(String command)	
+static Runtime getRuntime()	
+void exit(int status)	
+long freeMemory()	
+long totalMemory()	
+long maxMemory()
+
+	}
+	
+
+}
---
+++ util/test.java
@@ -0,0 +1,31 @@
+package util;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner,Calender,Date,SimpleDateFormat 등이 있음
+// 		Scanner 클래스 대표적인 메소드
+		String next();
+		int nextInt();
+		String nextLine();
+
+//		Calender 클래스 대표적인 메소드
+		int get(int field);
+		Date getTime();
+		void set();
+		void setTime();
+		void after();
+		void before();
+
+//		Date 클래스 대표적인 메소드
+		long getTime();
+		void setTime();
+		boolean after();
+		boolean before();
+		int compareTo();
+		boolean equals();
+		
+	}
+
+}
